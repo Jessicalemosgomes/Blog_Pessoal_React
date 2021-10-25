a9a77f30a7d96d0e08c755d8b774d3d4c4ec7795
jessica lemos gomes <lemosgomes24@gmail.com>
2021-10-25 19:16:07 -0300

    Introdução React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        
+        <>
+        <h1 className ='titulo'> Home</h1>
+        <img src='https://imgur.com/gallery/TBhdvu8' alt='Imagem tela tela' className='img'/>
+        </>
+    );
+}
+export default Home;
