f6068d9e971f0a6727006ec885b4c1b9f2b25d37
jessica lemos gomes <lemosgomes24@gmail.com>
2021-10-27 15:38:44 -0300

    Adcionando nova imagem ao Home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import {Typography, Box, Grid, Button} from '@material-ui/core';
 import './Home.css';
 
-function Home(){
-    return(
-        
+function Home() {
+    return (
         <>
-        <h1 className ='titulo'> Home</h1>
-        <img src='https://imgur.com/gallery/TBhdvu8' alt='Imagem tela tela' className='img'/>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://as1.ftcdn.net/v2/jpg/01/53/91/18/500_F_153911882_0TL4rN6j9tnIYkcRDinan8dXqFljeZZU.jpg" alt="" width="600px" height="300px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     );
 }
+
 export default Home;
