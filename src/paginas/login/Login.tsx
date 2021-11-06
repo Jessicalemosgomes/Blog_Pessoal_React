5e94df43efa8e69625011217025043c1d5ad4596
jessica lemos gomes <lemosgomes24@gmail.com>
2021-11-05 23:48:58 -0300

    Comunicação com o Back-end - Login

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,62 +1,78 @@
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
-import React, { ChangeEvent, useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { login } from '../../service/Service';
 import UserLogin from '../../models/UserLogin';
 import './Login.css';
 
 function Login() {
-
+    let history = useHistory();
+    const [token, setToken] = useLocalStorage('token');
     const [userLogin, setUserLogin] = useState<UserLogin>(
         {
             id: 0,
             usuario: '',
-            senha:'',
+            senha: '',
             token: ''
-
         }
         )
 
-        function updatedModel(e: ChangeEvent<HTMLInputElement>){
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
             setUserLogin({
-                ... userLogin,
+                ...userLogin,
                 [e.target.name]: e.target.value
             })
         }
+
+            useEffect(()=>{
+                if(token != ''){
+                    history.push('/home')
+                }
+            }, [token])
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+            try{
+                await login(`/usuarios/logar`, userLogin, setToken)
+
+                alert('Usuário logado com sucesso!');
+            }catch(error){
+                alert('Dados do usuário inconsistentes. Erro ao logar!');
+            }
+        }
+
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                        <TextField id='usuario' label='Usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
                                 <Button type='submit' variant='contained' color='primary'>
                                     Logar
                                 </Button>
-                            </Link>
                         </Box>
-
                     </form>
-
-                    <Box display='flex' justifyItems='center' marginTop={2}>
-                        <Box>
-                            <Typography variant='subtitle1' gutterBottom align ='center'>Ainda não possui uma conta?</Typography>
+                    <Box display='flex' justifyContent='center' marginTop={2}>
+                        <Box marginRight={1}>
+                            <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
-                       
-                        <Link to='/cadastrousuario'> 
-                        <Typography variant='subtitle1' gutterBottom align ='center' className='textos1'>Cadastre-se</Typography>
+                        <Link to='/cadastrousuario'>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1'>Cadastre-se</Typography>
                         </Link>
-                        
+                            
                     </Box>
                 </Box>
             </Grid>
-
             <Grid xs={6} className='imagem'>
 
             </Grid>
         </Grid>
     );
 }
+
 export default Login;
--- src/service/Service.ts
+++ src/service/Service.ts
@@ -4,12 +4,7 @@ export const api = axios.create({
     baseURL: 'https://bloggeneration.herokuapp.com'
 })
 
-    export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
-        const resposta = await api.post(url,dados)
-        setDado(resposta.data)
-    }
-
-    export const login = async(url: any,dados: any,setDado: any) => { 
-        const resposta = await api.post(url,dados)
-        setDado(resposta.data.token)
-    }
+  export const login = async(url: any,dados: any,setDado: any) => { 
+    const resposta = await api.post(url,dados)
+    setDado(resposta.data)
+  }
